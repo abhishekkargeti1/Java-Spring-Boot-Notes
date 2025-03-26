9d2a7195c41c072ba29d31af29258ca90931c3bd
AbhishekKargeti1 <abhishek.karegti1@gmail.com>
2025-03-27 00:32:46 +0530

    First 2 Project Set of Spring Boot

---
+++ FirstProjectSI/FirstProjectSI/src/main/java/com/FirstProjectSI/FirstProjectSiApplication.java
@@ -0,0 +1,13 @@
+package com.FirstProjectSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectSiApplication.class, args);
+	}
+
+}
