1efb35bf34507d2777937dcc0b1475260d4e0754
AbhishekKargeti1 <abhishek.karegti1@gmail.com>
2025-03-31 12:59:09 +0530

    JPA Configuration

--- ApplicationPropertiesFile/src/main/java/com/springBoot/controller/viewController.java
+++ ApplicationPropertiesFile/src/main/java/com/springBoot/controller/viewController.java
@@ -5,18 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class viewController {
-	
+
 	@RequestMapping("/")
 	public String getDefaultView() {
 		System.out.println("Hello World");
 		return "index";
 	}
 	
-
-	@RequestMapping("/home")
-	public String gethomeView() {
-		System.out.println("Hello World 2");
-		return "index";
+	@RequestMapping("/contact")
+	public String getContactView() {
+		System.out.println("Hello2 World");
+		return "contact";
 	}
-	
 }
