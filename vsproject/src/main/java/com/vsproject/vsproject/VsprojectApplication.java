e05857e990478090f1dea62f54e8e5c0c64caa51
AbhishekKargeti1 <abhishek.karegti1@gmail.com>
2025-03-31 10:32:44 +0530

    Auto Update

---
+++ vsproject/src/main/java/com/vsproject/vsproject/VsprojectApplication.java
@@ -0,0 +1,14 @@
+package com.vsproject.vsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsprojectApplication.class, args);
+		System.out.println("Hell I First Project Class");
+	}
+
+}
